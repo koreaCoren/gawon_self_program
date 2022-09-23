74ce9d29d6bc659b37083b1d1f3c35e130fca0a8
zyakal <pkd1426@gmail.com>
2022-09-23 14:35:37 +0900

    자가진단 틀

--- control.php
+++ control.php
@@ -1,7 +1,8 @@
 <?php
-$phone = $_POST['phone'];
 $gender = $_POST['gender'];
+$phone = $_POST['phone'];
 $child = $_POST['child'];
+$marrige = $_POST['marrige'];
 $creditor1 = $_POST['creditor1'];
 $creditor2 = $_POST['creditor2'];
 $creditor3 = $_POST['creditor3'];
@@ -13,7 +14,17 @@ $property5 = $_POST['property5'];
 $property6 = $_POST['property6'];
 $property7 = $_POST['property7'];
 $property8 = $_POST['property8'];
-$marrige = $_POST['marrige'];
+$income1 = $_POST['income1'];
+$income2 = $_POST['income2'];
+$income3 = $_POST['income3'];
+$income4 = $_POST['income4'];
+$income5 = $_POST['income5'];
+$income6 = $_POST['income6'];
+$income7 = $_POST['income7'];
+$income8 = $_POST['income8'];
+$income9 = $_POST['income9'];
+$income10 = $_POST['income10'];
+
 
 // $nm = $_POST['nm'];
 // $nm = $_POST['nm'];
@@ -21,7 +32,7 @@ $marrige = $_POST['marrige'];
 // $nm = $_POST['nm'];
 // $nm = $_POST['nm'];
 // $nm = $_POST['nm'];
-// $nm = $_POST['nm'];
+
 print '성별 : ' . $gender . '<br>';
 print '연락처 : ' . $phone . '<br>';
 print '자녀 유무 : '. $child . '<br>';
@@ -37,4 +48,15 @@ print '전월세-보증금,월세 : ' . $property5 . '<br>';
 print '전월세-보증금 담보대출금액 : ' . $property6 . '<br>';
 print '차량 소유 : ' . $property7 . '<br>';
 print '차량 시세 : ' . $property8 . '<br>';
+print '수입 형태 : ' . $income1 . '<br>';
+print '4대보험 가입 : ' . $income2 . '<br>';
+print '월평균 실수령액 : ' . $income3 . '<br>';
+print '영업_사업종류 : ' . $income4 . '<br>';
+print '영업_미납국세 : ' . $income5 . '<br>';
+print '영업_미납 건강/연금보험 : ' . $income6 . '<br>';
+print '영업_전년도 매출액 : ' . $income7 . '<br>';
+print '영업_전년도 영업이익 : ' . $income8 . '<br>';
+print '프리_직업 : ' . $income9 . '<br>';
+print '프리_월평균 수입 : ' . $income10 . '<br>';
+
 
