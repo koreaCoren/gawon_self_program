8d91e19868e549df713be429b29430943eb77c60
zyakal <pkd1426@gmail.com>
2022-09-27 14:49:42 +0900

    09.27

--- test.php
+++ test.php
@@ -1,15 +1,29 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form class='img_test image-form' id='store_img' action='store_photo.php' method='post' enctype='multipart/form-data'>
-    <input type='file' id='imageInput' name='img' accept='image/*' class='ui-input image-input__input' tabindex='0'>
-    <button type='submit'>등록</button>
-</form>
-</body>
-</html>
+<?php
+define('G5_MYSQL_HOST', 'gawon2021.cafe24.com');
+define('G5_MYSQL_USER', 'gawon2021');
+define('G5_MYSQL_PASSWORD', 'korea8988@@');
+define('G5_MYSQL_DB', 'gawon2021');
+define('G5_MYSQL_SET_MODE', true);
+define('G5_DATA_PATH', '');
+define('G5_DBCONFIG_FILE',  'dbconfig.php');
+define('G5_DOMAIN', 'C:\Apache24\gawon\data');
+$dbChar = "utf8";
+$dbName = G5_MYSQL_DB;
+$host = G5_MYSQL_HOST;
+$user = G5_MYSQL_USER;
+$pass = G5_MYSQL_PASSWORD;
+
+$dsn = "mysql:host={$host};dbname={$dbName};charset={$dbChar}";
+
+$pdo = new PDO($dsn, $user, $pass);
+// $sql = "SELECT * FROM test;";
+$sql = "INSERT INTO test (VALUE1, VALUE2, VALUE3) VALUES (1,2,3)";
+$stmt = $pdo->prepare($sql);
+$stmt->execute();
+
+$row = $stmt->fetch();
+
+print_r($row);
+
+?>
+
